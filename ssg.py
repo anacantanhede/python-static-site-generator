29daa5127de78fc1fa63e933a2e59d4ae7f87586
Ana Ferro Cantanhede <anacantanhede@gmail.com>
2021-01-10 14:52:30 +0100

    removed temporary variable

--- ssg.py
+++ ssg.py
@@ -3,7 +3,6 @@ from ssg.site import Site
 
 def main(source="content", dest="dist"):
     config = {"source": source, "dest" : dest}
-    s = Site(**config)
-    s.build()
+    Site(**config).build()
 
 typer.run(main)
